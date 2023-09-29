43053615ac1afd2a28c12b851a95f07e8e6b881c
Kshitij Arade <uttam.arade@automationedge.com>
2023-09-29 16:32:31 +0530

    All tasks

--- main.py
+++ main.py
@@ -7,45 +7,53 @@ import insightface
 from insightface.app import FaceAnalysis
 from insightface.data import get_image as ins_get_image
 
-#print(os.getcwd())
+# print(os.getcwd())
 img_path = 'C://Users//kshit//AppData//Roaming//Python//Python311//site-packages//insightface//data//images'
-#print(os.listdir(img_path))
 file_names = []
 
 for imgFile in os.listdir(img_path):
     tmp = imgFile.split('.', 1)
     file_names.append(tmp[0])
 
-print(len(file_names))
+# print(len(file_names))
 
-# app = FaceAnalysis(name='buffalo_l')
-# app.prepare(ctx_id=0, det_size=(640, 640))
-#
-# img = ins_get_image('ua')
-# faces = app.get(img)
+app = FaceAnalysis(name='buffalo_l')
+app.prepare(ctx_id=0, det_size=(640, 640))
+swapper = insightface.model_zoo.get_model('inswapper_128.onnx', download=True, download_zip=True)
 
-#img2 = ins_get_image('ua')
-#ufaces = app.get(img2)
+for template_face in ['tg1', 'ts', 'tt', 'ab']:
+    img = ins_get_image(template_face)
+    faces = app.get(img)
 
-#source_face = faces[0]
-# bbox = source_face['bbox']
-# bbox = [int(b) for b in bbox]
-# plt.imshow(img[bbox[1]:bbox[3], bbox[0]:bbox[2], ::-1])
-# plt.show()
+    #img2 = ins_get_image('ua')
+    #ufaces = app.get(img2)
 
-#usource_face = ufaces[0]
-#bbox = usource_face['bbox']
-#bbox = [int(b) for b in bbox]
-#plt.imshow(img2[bbox[1]:bbox[3], bbox[0]:bbox[2], ::-1])
-#plt.show()
+    source_face = faces[0]
+    # bbox = source_face['bbox']
+    # bbox = [int(b) for b in bbox]
+    # plt.imshow(img[bbox[1]:bbox[3], bbox[0]:bbox[2], ::-1])
+    # plt.show()
 
-# swapper = insightface.model_zoo.get_model('inswapper_128.onnx', download=True, download_zip=True)
-#
-# counter = '65'
-# pimg = ins_get_image(counter)
-# pfaces = app.get(pimg)
-# pimg = swapper.get(pimg, pfaces[0], source_face, paste_back=True)
-# #pimg = swapper.get(pimg, pfaces[1], usource_face, paste_back=True)
-# plt.imshow(pimg[:, :, ::-1])
-# plt.show()
-# cv2.imwrite(f"{os.getcwd()}/output/tg{counter}.jpg", pimg)
+    # usource_face = ufaces[0]
+    # bbox = usource_face['bbox']
+    # bbox = [int(b) for b in bbox]
+    # plt.imshow(img2[bbox[1]:bbox[3], bbox[0]:bbox[2], ::-1])
+    # plt.show()
+
+    # swapper = insightface.model_zoo.get_model('inswapper_128.onnx', download=True, download_zip=True)
+
+    for i, target_file in enumerate(file_names):
+        if target_file in ['ua', 'tg', 'tg1', 'tg2', 'ts', 'tt', 'ab', 'pk', 'ss']:
+            print(f'template file {target_file}')
+        else:
+            target_image = ins_get_image(target_file)
+            target_face = app.get(target_image)
+            target_image = swapper.get(target_image, target_face[0], source_face, paste_back=True)
+            # target_image = swapper.get(target_image, target_face[1], usource_face, paste_back=True)
+            # plt.imshow(target_image[:, :, ::-1])
+            # plt.show()
+            cv2.imwrite(f"{os.getcwd()}/output/{template_face}/{i}.jpg", target_image)
+
+    print(f'Completed for {template_face}')
+
+print('All tasks complete')
